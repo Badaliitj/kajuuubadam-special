9d3fa8d6757f7510f39621024b5bd09ab3173ebb
Badaliitj <b25mt1009@iitj.ac.in>
2026-06-15 22:42:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-const PASSWORD='BadalKashuu#103';
-function unlock(){if(document.getElementById('pwd').value===PASSWORD){login.style.display='none';site.style.display='block';}else{msg.innerText='Wrong Password ❤️';}}
-function showJourney(){journey.style.display='block';journey.scrollIntoView({behavior:'smooth'});}
-function playVideo(){playBtn.style.display='none';videoFrame.style.display='block';videoFrame.src='https://drive.google.com/file/d/1hBeBB0j-lXvJLU_uhUhjxub4rqSQ2bBg/preview';}
